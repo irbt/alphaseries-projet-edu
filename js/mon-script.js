e763493bddc11bc5c7356cd92ebd35443de3a5e4
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-04-30 22:53:11 +0200

    Add files via upload

---
+++ js/mon-script.js
@@ -0,0 +1,24 @@
+(function( $ ) { // début de fichier
+
+
+    $('#menuSelector').on("click", function(){
+        $('#mobileNav').slideToggle(400);
+    });
+
+    $('#commencez').on("click", function(){
+        $('#placeholder').css( "display", "block" );
+        $('#commencez').css( "display" , "none" );
+        return false;
+    });
+
+
+    $('#commencez').on("mouseover", function(){
+        $('#imgHover').css( "display", "none" );
+        $('#imgHoverbis').css( "display", "block" );
+        // $('#commencez').css( "display" , "none" );
+        return false;
+    });
+
+    clearInterval()
+commencez
+})( jQuery ); // Fin de fichier
